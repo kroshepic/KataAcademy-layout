b5d8c6a2e301a126dbd62495b94c3e7cfd1f5cfb
Azat Xalitov <xalitov.work@mail.ru>
2024-09-19 11:12:35 +0300

    Commit All

---
+++ src/js/brands.js
@@ -0,0 +1,7 @@
+// Добавим открытие модального окна для брендов при клике
+
+import { main } from './nav-links.js';
+import { brands, brandsItem } from './swiper-brands.js';
+
+//console.log(brandsItem);
+
